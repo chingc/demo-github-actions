6e1b73662bd83ecfe3ebde8ec57fa96a6e666e7b
Ching C <ching64@proton.me>
2025-05-12 18:11:26 -0400

    New tests (#12)
    * Move test
    
    * Add tests to demo parallel testing
    
    * Cleanup

---
+++ tests/dir3/test_parallel_5.py
@@ -0,0 +1,11 @@
+import time
+
+def test_parallel_5a():
+    # Used to demonstrate parallel testing.
+    time.sleep(1)
+    assert True
+
+def test_parallel_5b():
+    # Used to demonstrate parallel testing.
+    time.sleep(1)
+    assert True
